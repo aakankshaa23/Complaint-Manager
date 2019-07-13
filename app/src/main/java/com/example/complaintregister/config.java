2547716ef83d9a129c8420f1819f5228840235a6
aakankshaa23 <taakanksha99@gmail.com>
2019-07-13 10:45:17 +0530

    Complaint reg and status done

---
+++ app/src/main/java/com/example/complaintregister/config.java
@@ -0,0 +1,6 @@
+package com.example.complaintregister;
+
+public class config {
+    public static final String EMAIL ="taakanksha99@gmail.com";
+    public static final String PASSWORD ="giveyorbest";
+}
