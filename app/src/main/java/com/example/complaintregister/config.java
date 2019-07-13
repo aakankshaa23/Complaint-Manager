fb66fd48615803610e8ffa4d71d765a949c097a0
aakankshaa23 <37452934+aakankshaa23@users.noreply.github.com>
2019-07-13 10:55:37 +0530

    .

--- app/src/main/java/com/example/complaintregister/config.java
+++
@@ -1,6 +0,0 @@
-package com.example.complaintregister;
-
-public class config {
-    public static final String EMAIL ="taakanksha99@gmail.com";
-    public static final String PASSWORD ="giveyorbest";
-}
